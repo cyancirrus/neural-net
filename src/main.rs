da72fa97c2678c7ef3a996c484663117ab4af587
autumn <ravenecho@Ravens-MacBook-Pro.local>
2024-12-08 16:53:30 -0600

    trying to think how to stitch

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,57 @@ mod list;
 mod tree;
 pub use list::List;
 pub use tree::Tree;
+extern crate rand;
+use rand::Rng;
+
+pub struct Neuron {
+    weights: Vec<f64>,
+    bias: f64,
+}
+
+pub struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+pub struct Network {
+    layers: Vec<Layer>,
+}
+fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + (-x).exp())
+}
+
+impl Neuron {
+    pub fn new(size: usize) -> Self {
+        let mut rng = rand::thread_rng();
+        Neuron {
+            weights: (0..size).map(|_| rng.gen_range(-1.0..1.0)).collect(),
+            bias: rng.gen_range(-1.0..1.0),
+        }
+    }
+    pub fn forward(&self, inputs: &Vec<f64>) -> f64 {
+        let dotproduct: f64 = self
+            .weights
+            .iter()
+            .zip(inputs.iter())
+            .map(|(w, v)| w * v)
+            .sum();
+        sigmoid(dotproduct + self.bias)
+    }
+}
+
+impl Layer {
+    pub fn new(size: usize) -> Self {
+        Layer {
+            neurons: (0..size).map(|_| Neuron::new(size)).collect(),
+        }
+    }
+}
+
+// impl Network {
+//     fn new(size:Vec<usize>) -> Self {
+
+//     }
+// }
 
 fn main() {
     let mut tree = Tree::new();
