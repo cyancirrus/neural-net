c5fa3591dd90c7427a884bcd9d8500ee28f83b82
autumn <ravenecho@Ravens-MacBook-Pro.local>
2025-02-17 11:37:22 -0600

    think this is the final bug?

--- src/main.rs
+++ src/main.rs
@@ -51,16 +51,18 @@ fn proto_tensor_mult(blocksize:usize, x:NdArray, y:NdArray) -> NdArray {
             for k in 0..(x_cols + blocksize - 1) / blocksize{
                 let k_bound = (x_cols + 1) / blocksize;
                 println!("K: {}, kbound: {}", k, k_bound);
-                for ii in 0..blocksize - (i + blocksize) % x_rows % blocksize {
-                    for jj in 0..blocksize - (j + blocksize) % y_cols % blocksize {
-                        for kk in 0..blocksize {
+                for ii in 0..blocksize.min(x_rows - i) {
+                    for jj in 0..blocksize.min(y_cols -j){
+                        for kk in 0..blocksize.min(x_cols - k * blocksize) {
+                // for ii in 0..blocksize - (i + blocksize) % x_rows % blocksize {
+                //     for jj in 0..blocksize - (j + blocksize) % y_cols % blocksize {
+                //         for kk in 0..blocksize {
                             if k * blocksize + kk  >= x_cols {
                             } else {
                                 counter+=1;
                                 let index = (i + ii) * y_cols + jj + j;
                                 let x_index = (i + ii ) * x_cols + k * blocksize + kk;
                                 let y_index =  (k * blocksize + kk) * y_cols + jj + j;
-
                                 let value ={
                                     x.data[x_index]
                                     * y.data[y_index]
@@ -87,75 +89,75 @@ fn generate_matrix(rows: usize, cols: usize) -> NdArray {
     NdArray::new(vec![rows, cols], data)
 }
 
-fn main() {
-    // let i = 3;  // Number of rows in x
-    // let j = 4;  // Number of columns in x, also rows in y
-    // let k = 5;  // Number of columns in y
+// fn main() {
+//     // let i = 3;  // Number of rows in x
+//     // let j = 4;  // Number of columns in x, also rows in y
+//     // let k = 5;  // Number of columns in y
 
-    // let x = generate_matrix(i, j);
-    // let y = generate_matrix(j, k);
+//     // let x = generate_matrix(i, j);
+//     // let y = generate_matrix(j, k);
 
-    // let blocksize = 4; // Test with different block sizes
-    // let result = proto_tensor_mult(blocksize, x, y);
-
-
-    let x = NdArray::new(vec![2, 3], vec![
-        1.0, 2.0, 3.0,
-        4.0, 5.0, 6.0,
-    ]);
-
-    let y = NdArray::new(vec![3, 2], vec![
-        7.0, 8.0,
-        9.0, 10.0,
-        11.0, 12.0,
-    ]);
-
-    let result = proto_tensor_mult(4, x, y);
-    println!("{:?}", result);
+//     // let blocksize = 4; // Test with different block sizes
+//     // let result = proto_tensor_mult(blocksize, x, y);
 
 
-    // println!("{:?}", result);
-}
+//     let x = NdArray::new(vec![2, 3], vec![
+//         1.0, 2.0, 3.0,
+//         4.0, 5.0, 6.0,
+//     ]);
+
+//     let y = NdArray::new(vec![3, 2], vec![
+//         7.0, 8.0,
+//         9.0, 10.0,
+//         11.0, 12.0,
+//     ]);
+
+//     let result = proto_tensor_mult(5, x, y);
+//     println!("{:?}", result);
 
 
-
-
-// fn main () {
-//     let x_dim = 3;
-//     let y_dim = 3;
-
-//     let test = usize::pow(x_dim, 2);
-    
-//     let mut x:Vec<f32> = Vec::with_capacity(usize::pow(x_dim, 2));
-//     let mut y:Vec<f32> = Vec::with_capacity(usize::pow(y_dim, 2));
-//     let mut dims:Vec<usize> = Vec::with_capacity(2);
-
-//     let mut j = 0;
-//     for i in 0..usize::pow(x_dim, 2) {
-//         j = i / x_dim;
-//         if (i - j) % x_dim == 0 {
-//             x.push(1_f32)
-//         } else {
-//             x.push(0_f32);
-//         }
-//     }
-//     for i in 0..usize::pow(y_dim, 2) {
-//         y.push(i as f32)
-//     }
-
-//     for _ in 0..2 {
-//         dims.push(x_dim)
-//     }
-
-//     let x_array = NdArray::new(dims.clone(), x);
-//     let y_array = NdArray::new(dims.clone(), y);
-
-//     println!("X array: {:?}", x_array);
-//     println!("y array: {:?}", y_array);
-//     let result = proto_tensor_mult(4, x_array, y_array);
-//     println!("Output result\n {:?}", result); 
-
-//     // let A = Matrix::new(4, 1, others);
-//     // let transpose = transpose(A);
-//     // println!("Transpose: {:?}", transpose);
+//     // println!("{:?}", result);
 // }
+
+
+
+
+fn main () {
+    let x_dim = 5;
+    let y_dim = 5;
+
+    let test = usize::pow(x_dim, 2);
+    
+    let mut x:Vec<f32> = Vec::with_capacity(usize::pow(x_dim, 2));
+    let mut y:Vec<f32> = Vec::with_capacity(usize::pow(y_dim, 2));
+    let mut dims:Vec<usize> = Vec::with_capacity(2);
+
+    let mut j = 0;
+    for i in 0..usize::pow(x_dim, 2) {
+        j = i / x_dim;
+        if (i - j) % x_dim == 0 {
+            x.push(1_f32)
+        } else {
+            x.push(0_f32);
+        }
+    }
+    for i in 0..usize::pow(y_dim, 2) {
+        y.push(i as f32)
+    }
+
+    for _ in 0..2 {
+        dims.push(x_dim)
+    }
+
+    let x_array = NdArray::new(dims.clone(), x);
+    let y_array = NdArray::new(dims.clone(), y);
+
+    println!("X array: {:?}", x_array);
+    println!("y array: {:?}", y_array);
+    let result = proto_tensor_mult(4, x_array, y_array);
+    println!("Output result\n {:?}", result); 
+
+    // let A = Matrix::new(4, 1, others);
+    // let transpose = transpose(A);
+    // println!("Transpose: {:?}", transpose);
+}
