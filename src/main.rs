e55953f1a80fffb7640410b80cbc876ef8c5377d
autumn <ravenecho@Ravens-MacBook-Pro.local>
2025-02-26 21:37:44 -0600

    grossest hacks, need to rethink on how to do this, but at least this is valid and works!

--- src/main.rs
+++ src/main.rs
@@ -40,16 +40,11 @@ fn lu_factorization(x: &blas::NdArray) -> (NdArray, NdArray) {
     )
 }
 
-fn proto_householder(mut x: &blas::NdArray) -> NdArray {
-    // let magnitude = math::magnitude(&x.data);
-    // let u = x.data + magnitude *
-    todo!()
-}
-
 // fn householder_matrix(mut x: &[f32]) -> NdArray {
-fn householder_matrix(mut x: &[f32]) -> (f32, Vec<f32>) {
+fn householder_params(mut x: &[f32]) -> (f32, Vec<f32>) {
     let length = x.len();
     assert!(length > 0, "needs to have non-zero length");
+    println!("X: {:?}", x);
     let dims = vec![length; 2];
     let data = vec![0_f32; length * length];
     let mut householder = NdArray::new(dims, data);
@@ -60,40 +55,40 @@ fn householder_matrix(mut x: &[f32]) -> (f32, Vec<f32>) {
     u[0] += math::magnitude(&u) * x[0].signum();
     let magnitude_squared = math::dot_product(&u, &u);
     for i in 0..length {
-        // initialize the identity matrix
         householder.data[i * length + i] = 1_f32;
     }
-    // let projection = math::outer_product(u);
-    // householder.data.par_iter_mut()
-    //     .zip(projection.par_iter())
-    //     .for_each(|(h, p)| *h = *h - 2_f32 * p / magnitude_squared);
-    // householder
     (2_f32 / magnitude_squared, u)
 }
 
-fn hoseholder_factor(mut x: NdArray) -> NdArray {
+
+fn householder_factor(mut x: NdArray) -> NdArray {
     let rows = x.dims[0];
     let cols = x.dims[1];
+    let mut new = x.data.clone();
     
-    for j in 0..cols {
-        let column_vector = (0..rows).into_par_iter().map(|r| x.data[r*cols + j]).collect::<Vec<f32>>();
-        let (b, u) = householder_matrix(&column_vector);
-        println!("householder vector: {:?}", u);
-        x.data[j*cols + j] *= 1_f32 - u[0].powi(2)*b;
-        for i in j..rows {
-            for k in i..cols {
-                if i == k {
-                    x.data[i*cols + j] += x.data[k*cols + j] * (1_f32 - b *u[i].powi(2));
-                } else {
-                    x.data[i*cols + j] -= x.data[k*cols + j] * b * u[k] * u[i];
-                    x.data[k*cols + j] = 0_f32;
+    // for o in 0..cols.min(rows) {
+    for o in 0..cols.min(rows) {
+        let column_vector = (o..rows).into_par_iter().map(|r| new[r*cols + o]).collect::<Vec<f32>>();
+        let (b, u) = householder_params(&column_vector);
+        println!("Column vector: {:?}", column_vector);
+        println!("Householder vector: {:?}, row: {}", u, o);
+        // for i in o..rows - o {
+        // for i in o..rows - o  {
+        // for i in o..rows  {
+        for i in o..rows {
+            for j in o..cols {
+                for k in o..rows {
+                    println!("i: {}, j: {}, k: {}", i, j, k);
+                    println!("source: ({}, {}), target: ({}, {}), destination: ({}, {})", i, k, k, j, i, j);
+                    new[i*cols + j] -= x.data[k*cols + j] * b * u[i-o] * u[k-o];
                 }
             }
+            let logs = NdArray::new(x.dims.clone(), new.clone());
+            println!("{}th change: {:?}", i+1, logs);
         }
-        println!("data: {:?}", x);
-
+        x.data = new.clone();
     }
-    x
+    NdArray::new(x.dims, new)
 }
 
 
@@ -120,17 +115,27 @@ fn main() {
     dims[0] = 3;
     dims[1] = 3;
     data[0] = 0_f32;
-    data[1] = 4_f32;
+    data[1] = 1_f32;
     data[2] = 1_f32;
-    data[3] = 4_f32;
-    data[4] = 1_f32;
-    data[5] = 1_f32;
+    data[3] = 1_f32;
+    data[4] = 2_f32;
+    data[5] = 3_f32;
     data[6] = 1_f32;
     data[7] = 1_f32;
     data[8] = 1_f32;
+    // data[0] = 0_f32;
+    // data[1] = 1_f32;
+    // data[2] = 1_f32;
+    // data[3] = 4_f32;
+    // data[4] = 2_f32;
+    // data[5] = 3_f32;
+    // data[6] = 1_f32;
+    // data[7] = 1_f32;
+    // data[8] = 1_f32;
     let x = blas::NdArray::new(dims, data.clone());
+    println!("input matrix {:?}", x);
     // let fact = proto_householder(&ndarray);
-    let h = hoseholder_factor(x);
+    let h = householder_factor(x);
     println!("hoseholder factor: {:?}", h);
 
     // let test = blas::tensor_mult(1, &h, &x);
